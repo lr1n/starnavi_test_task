ec1b725b2ca3b70cb6fb17cb4e8ff08dad2656c8
lr1n <sko0hlol@gmail.com>
2021-08-18 19:03:35 +0200

    Changed like_analytics function

--- social_network_app/utils.py
+++ social_network_app/utils.py
@@ -1,12 +1,12 @@
 def like_analytics(serializer):
     likes_per_day = {}
-    for el in serializer.data:
-        k = el['created_at'][:10]
-        likes_per_day[k] = []
+    for data in serializer.data:
+        k = data['created_at'][:10]
+        likes_per_day[k] = 0
     for el in serializer.data:
         k = el['created_at'][:10]
         if k in likes_per_day:
-            likes_per_day[k].append(el)
+            likes_per_day[k] += 1
     response = {
         'total_likes': len(serializer.data),
         'likes_per_day': likes_per_day
